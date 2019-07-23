ececca4851465d087cb726e5d26f4db6edb82019
冯立 <49088278+pleasetakeup@users.noreply.github.com>
2019-07-23 13:28:15 +0800

    Update readVoc.ino

--- example/readVoc/readVoc.ino
+++ example/readVoc/readVoc.ino
@@ -1,6 +1,6 @@
 /*!
    @file readVoc.ino
-   @brief 读取空气中VOC的浓度和芯片的版本号，单位是勒克斯PPM
+   @brief 读取空气中VOC的浓度和芯片的版本号，单位是PPM
    @n 实验现象：我们在开始前读取一次芯片的版本号，以便了解芯片，然后是每2秒读取一次voc浓度，并打印到串口
    @n 用不同的浓度的测试气体，可以得到不同的采集结果
    @n 传感器的分辨率是0.1ppm，传感器的量程时0~100PPM 所以只能测量较窄的范围。
@@ -21,7 +21,7 @@ void setup() {
   Serial.begin(115200);
   //程序开始前会检测一下IIC是否能正常通信,begin()函数返回0则表示能正常通信
   while (AGS01DB.begin() != 0) {
-    Serial.println("初始化芯片失败，请确认芯片连接是否正确");
+    Serial.println("初始化芯片失败，请确认芯片连线是否正确");
     delay(1000);
   }
   Serial.print("the version of AGS01DB:");
@@ -30,12 +30,12 @@ void setup() {
     函数名称：readSensorVersion()
     @brief 芯片的版本号.
   */
-  Serial.println(AGS01DB.readSensorVersion(), HEX);
+  Serial.println(/*版本号=*/AGS01DB.readSensorVersion(), HEX);
 
 }
 void loop() {
 
-  //每两秒读取一次voc值，不能更短，因为改芯片的采样周期 >=2s，小于两秒则会读到上一次采样的值.
+  //每两秒读取一次voc值，不能更短，因为该芯片的采样周期 >=2s，小于两秒则会读到上一次采样的值.
   //若间隔较长时间使用该模块，那么最好预热120s，才会读到较准确的值.
   Serial.print("the concentration of Voc:");
   /*
